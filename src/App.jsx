a0b4cd4d09db0be1ef7af09d0356cd826085ed37
Ayobami-ANDROID <ajetunmobiayobami19@gmail.com>
2024-08-08 00:41:50 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import Highlight from './components/Highlight'
+
+function App() {
+  
+
+  return (
+    <main className='bg-black'>
+      <Navbar/>
+      <Hero/>
+      <Highlight/>
+      
+    </main>
+  )
+}
+
+export default App
---
+++ src/components/Hero.jsx
@@ -0,0 +1,24 @@
+import gsap from "gsap"
+import { useGSAP } from "@gsap/react"
+import { heroVideo,smallHeroVideo } from "../utils"
+
+const Hero = () => {
+    useGSAP(() => {
+        gsap.to("#hero",{opacity: 1, delay:1.5})
+    },[])
+  return (
+    <section className='w-full nav-height bg-black relative'>
+        <div className='h-5/6 w-full flex-center flex-col'>
+            <p id="hero" className='hero-title'>iphone 15 pro</p>
+            <div className="md:w-10/12 w-9/12">
+                <video>
+                    <source src={videoSrc}/>
+                </video>
+            </div>
+        </div>
+
+    </section>
+  )
+}
+
+export default Hero
